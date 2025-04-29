e5569e1a70565946f2c6657460021b01ed8502ad
DavidMoralesz4 <jd3285201@gmail.com>
2025-04-29 16:57:56 -0500

    add: modify getCategoryController

--- server/src/controllers/categoryControllers.ts
+++ server/src/controllers/categoryControllers.ts
@@ -1,27 +1,38 @@
-import { Request, Response } from "express"
-import { createCategoryService } from "../services/categoryService"
+import { Request, Response } from "express";
+import {
+  createCategoryService,
+  getCategoryService,
+} from "../services/categoryService";
 
-export const getCategoryController = (req: Request, res: Response) => {
-    try {
-        
-    } catch (error) {
-        
+export const getCategoryController = async (req: Request, res: Response) => {
+  try {
+    const categories = await getCategoryService();
+
+    res.status(200).json({
+      success: true,
+      message: "Categorias",
+      data: categories,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
     }
-}
+  }
+};
 
-export const createCategoryController = async(req: Request, res: Response) => {
-    const {name} = req.body
-    
-    try {
-        await createCategoryService(name)
+export const createCategoryController = async (req: Request, res: Response) => {
+  const { name } = req.body;
 
-        res.status(201).json({
-            success: true,
-            message: 'Categoria creada con exito!.'
-        })
-    } catch (error: unknown) {
-        if(error instanceof Error) {
-            res.status(500).json({message: error.message});
-        }
+  try {
+    await createCategoryService(name);
+
+    res.status(201).json({
+      success: true,
+      message: "Categoria creada con exito!.",
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
     }
-}
+  }
+};
