94fd37eeb8665e48dd0400638cd254574fac0da6
DavidMoralesz4 <jd3285201@gmail.com>
2025-05-08 16:18:35 -0500

    feat: NavSection component with basic logic \ settings other components

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -2,30 +2,36 @@ import AddProductComponent from "@/components/AddProductComponent";
 import SearchComponent from "@/components/SearchComponent";
 import React from "react";
 import SideCategory from "./sideCategory/SideCategory";
-import {auth} from '@/auth'
+import { auth } from "@/auth";
 
 export default async function DashboardPage() {
   const session = await auth();
-  
-  if(!session) {
+
+  if (!session) {
     return <div>No autorizado</div>;
   }
-  
+
   return (
-    <div className="w-full px-4 md:px-6">
-      <h1 className="text-3xl font-bold mb-4 text-black">Inventario</h1>
-      <p className="text-black mb-5">Todos los productos</p>
-      <div className="flex flex-col lg:flex-row gap-6">
-        <div className="flex-1">
-          <div className="flex flex-col md:flex-row gap-4 items-start">
-            <SearchComponent />
-            <AddProductComponent />
+    <>
+      <div className="w-full px-4 md:px-6">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          Tu Inventario
+        </h1>
+
+        <div className="absolute left-237 top-[50px] z-50 md:left-237 xl:left-360 xl:top-[70px]">
+          <SideCategory />
+        </div>
+
+        <p className="text-black mb-5">Todos los productos</p>
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="">
+            <div className="flex flex-col md:flex-row gap-4 items-start ">
+              <SearchComponent />
+              <AddProductComponent />
+            </div>
           </div>
         </div>
-        {/* <div className="lg:w-64">
-          <SideCategory />
-        </div> */}
       </div>
-    </div>
+    </>
   );
 }
