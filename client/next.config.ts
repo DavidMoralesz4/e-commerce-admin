63ae6030c124a4d26d136f36a203f142e7fee1b0
DavidMoralesz4 <jd3285201@gmail.com>
2025-05-09 23:13:46 -0500

    feat: product-card with styles basic \ settings side-category and create cancel-btn component

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    remotePatterns: [new URL('https://www.moov.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw76f76351/products/NIDC0774-141/NIDC0774-141-1.JPG')],
+  },
 };
 
 export default nextConfig;
--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,13 +1,15 @@
-import React, { ReactNode } from 'react'
-import SideBar from './sidebar/SideBar'
+import React, { ReactNode } from "react";
+import SideBar from "./sidebar/SideBar";
+import SideCategory from "./sideCategory/SideCategory";
 
-export default function DashboardLayout({children}: {children: ReactNode}) {
+export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='flex min-h-screen bg-gray-950'>
-        <SideBar/>
-        <main className='flex-1 bg-gray-100 p-8 m-2 rounded-[5px]'>
-            {children}
-        </main>
+    <div className="flex min-h-screen bg-gray-950">
+      <SideBar />
+      <main className="flex bg-gray-100 p-8 m-2 rounded-[5px] w-auto">
+        {children}
+        <SideCategory />
+      </main>
     </div>
-  )
+  );
 }
