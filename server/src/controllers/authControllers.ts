3c5a65e76f78d39b0ada4b71be9e21395b1846bb
DavidMoralesz4 <jd3285201@gmail.com>
2025-05-16 20:34:36 -0500

    adjustin cookie

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,68 +1,68 @@
 import { Request, Response } from "express";
 import { loginService, registerService } from "../services/authService";
 
-export const loginController = async(req: Request, res: Response) => {
-    const {email, password} = req.body
+export const loginController = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    try {
-        const { token, user } = await loginService(email, password);
+  try {
+    const { token, user } = await loginService(email, password);
 
-        res.cookie('tokenkeysecret', token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-        })
+    res.cookie("tokenkeysecret", token, {
+      httpOnly: true,
+      secure: true, // solo si está en HTTPS, como ahora
+      sameSite: "none", // <--- obligatorio para cross-site
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 
-        res.status(200).json({
-            success: true,
-            message: 'Ingresaste con exito!',
-            data: user
-        })
-    } catch (error: unknown) {
-        if(error instanceof Error){
-            res.status(500).json({
-                success: false,
-                message: error.message
-            })
-        }
+    res.status(200).json({
+      success: true,
+      message: "Ingresaste con exito!",
+      data: user,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
+  }
+};
 
-export const registerController = async(req: Request, res: Response) => {
-    const {firstName, lastName, document, email, password} = req.body
+export const registerController = async (req: Request, res: Response) => {
+  const { firstName, lastName, document, email, password } = req.body;
 
-    try {
-        
-        await registerService(firstName, lastName, document, email, password);
+  try {
+    await registerService(firstName, lastName, document, email, password);
 
-        res.status(201).json({
-            success: true,
-            message: 'Te registraste con exito!'
-        })
-
-    } catch (error: unknown) {
-        if(error instanceof Error){
-            res.status(500).json({
-                success: false,
-                message: error.message
-            })
-        }
+    res.status(201).json({
+      success: true,
+      message: "Te registraste con exito!",
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
+  }
+};
 
 export const logoutController = (_: Request, res: Response) => {
-    try {
-        res.clearCookie('tokenkeysecret')
-        res.status(200).json({
-            success: true,
-            message: 'Has cerrado sesion!'
-        })
-    } catch (error: unknown) {
-        if(error instanceof Error){
-            res.status(500).json({
-                success: false,
-                message: error.message
-            })
-        }
+  try {
+    res.clearCookie("tokenkeysecret");
+    res.status(200).json({
+      success: true,
+      message: "Has cerrado sesion!",
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
+  }
+};
